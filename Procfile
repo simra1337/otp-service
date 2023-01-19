0461f680c243052f202d9cad6765b8e4d304c644
Mohd Simra <mohd.simra@tothenew.com>
2023-01-19 14:40:35 +0530

    added flask functionality

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn otp-service:app
--- otp-service.py
+++ otp-service.py
@@ -1,3 +1,4 @@
+from flask import Flask, session
 import random
 from twilio.rest import Client
 from dotenv import load_dotenv
@@ -5,19 +6,50 @@ import os
 
 load_dotenv()
 
+app = Flask(__name__)
+app.secret_key = os.environ.get('API_SECRET_KEY') # any random string
+
 # Your Account Sid and Auth Token from twilio.com/console
 account_sid = os.environ.get('ACCOUNT_SID')
 auth_token = os.environ.get('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-# Generate a random OTP
-otp = random.randint(100000, 999999)
+@app.route('/sendOtp/<mobile_number>')
+def send_otp(mobile_number):
+    # Generate a random OTP
+    otp = random.randint(100000, 999999)
+    # Send the OTP via SMS
+    message = client.messages.create(
+        body='Your OTP is: ' + str(otp),
+        from_=os.environ.get('FROM'),
+        to='+91' + mobile_number
+    )
+    session["mobile_number"] = mobile_number
+    session["otp"] = otp
+    print(message.sid)
+    return "OTP sent to {}".format(mobile_number)
 
-# Send the OTP via SMS
-message = client.messages.create(
-    body='Your OTP is: ' + str(otp),
-    from_=os.environ.get('FROM'),
-    to=os.environ.get('TO')
-)
+@app.route('/otp/<otp>')
+def handle_otp(otp):
+    # get the mobile number from session
+    mobile_number = session.get("mobile_number")
+    # get the otp from session
+    stored_otp = session.get("otp")
 
-print(message.sid)
+    if stored_otp:
+        if stored_otp == int(otp):
+            return "Valid otp"
+            # OTP is valid
+            # Perform desired action
+            pass
+        else:
+            return "Invalid otp"
+            # OTP is invalid
+            pass
+    else:
+        return "Otp Expired"
+        pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
