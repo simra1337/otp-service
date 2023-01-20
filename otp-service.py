a82ee9ce665f28e36447bb3cc8cecfbcd7fa9ac2
Mohd Simra <mohd.simra@tothenew.com>
2023-01-20 15:35:56 +0530

    storing otp in db instead of session

--- otp-service.py
+++ otp-service.py
@@ -1,10 +1,12 @@
 from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
 import random
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 from flask import redirect
 from flask import render_template
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -16,9 +18,19 @@ account_sid = os.environ.get('ACCOUNT_SID')
 auth_token = os.environ.get('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-@app.route('/')
-def open_index():
-    return redirect('/homepage')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+db = SQLAlchemy(app)
+
+class OTP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mobile_number = db.Column(db.String(20), nullable=False)
+    otp = db.Column(db.String(6), nullable=False)
+    expires_at = db.Column(db.DateTime, nullable=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/sendOtp/<mobile_number>')
 def send_otp(mobile_number):
@@ -30,26 +42,28 @@ def send_otp(mobile_number):
         from_=os.environ.get('FROM'),
         to='+91' + mobile_number
     )
-    session["mobile_number"] = mobile_number
-    session["otp"] = otp
+    otp_object = OTP(mobile_number=mobile_number,otp=otp,expires_at=datetime.now() + timedelta(minutes=5))
+    db.session.add(otp_object)
+    db.session.commit()
     print(message.sid)
     return redirect('/otp')
 
 @app.route('/otp/<otp>')
 def handle_otp(otp):
-    # get the mobile number from session
-    mobile_number = session.get("mobile_number")
-    # get the otp from session
-    stored_otp = session.get("otp")
-
-    if stored_otp:
-        if stored_otp == int(otp):
+    otp_data = OTP.query.filter_by(otp=otp).first()
+    if otp_data:
+        if datetime.now() < otp_data.expires_at:
             return "OTP is valid"
         else:
-            return "Invalid OTP"
+            #OTP.query.filter_by(otp=otp).delete()
+            #db.session.commit()
+            return "OTP has expired"
     else:
-        return "OTP has expired"
+        return "Invalid OTP"
 
+@app.route('/')
+def open_index():
+    return redirect('/homepage')
 
 @app.route('/homepage')
 def homepage():
